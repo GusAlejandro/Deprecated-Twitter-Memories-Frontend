baea8e2fa4ff92d195e6c99e2575cacd21f16398
Gustavo Cornejo <gcornejo85@gmail.com>
2018-05-14 22:50:31 -0700

    removing old dataa

--- src/main.js
+++ src/main.js
@@ -15,21 +15,3 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
-
-// Initialize Firebase
-// let FBconfig = {
-//   apiKey: 'AIzaSyA8RWjHy25fGRtOFwGZ4XlOVSycQFs4-Qs',
-//   authDomain: 'twitter-memories-file-storage.firebaseapp.com',
-//   databaseURL: 'https://twitter-memories-file-storage.firebaseio.com',
-//   projectId: 'twitter-memories-file-storage',
-//   storageBucket: 'twitter-memories-file-storage.appspot.com',
-//   messagingSenderId: '182189708225'
-// }
-// router.beforeEach((to, from, next) => {
-//   const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
-//   if (requiresAuth) {
-//     next('/')
-//   } else {
-//     next()
-//   }
-// })
